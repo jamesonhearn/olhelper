6d2e7ff6b396bc6ce81d89bfc4f2adb16bf8f97c
jamesonhearn <jamesonhearn@microsoft.com>
2025-08-23 13:37:32 -0700

    updated

--- web/msalConfig.js
+++ web/msalConfig.js
@@ -1,9 +1,9 @@
 // TODO: fill in your Entra app registration details
 const msalConfig = {
   auth: {
-    clientId: "YOUR_CLIENT_ID", // e.g., 00000000-0000-0000-0000-000000000000
-    authority: "https://login.microsoftonline.com/YOUR_TENANT_ID_OR_NAME", //replace
-    redirectUri: "https://YOUR_HOST/web/auth.html" //replace
+    clientId: "17d1755f-2fe3-482a-ad96-9867c75e8146", // e.g., 00000000-0000-0000-0000-000000000000
+    authority: "https://login.microsoftonline.com/", //replace
+    redirectUri: "https://red-wave-057987a0f.2.azurestaticapps.net/auth.html" //replace
   },
   cache: { cacheLocation: "sessionStorage", storeAuthStateInCookie: false }
 };
